959f450af0c9ad8db110f384261dbe9abc86baa6
shri0612 <shri2178499@gmail.com>
2025-11-19 19:18:20 +0000

    coverage fix11

--- accounts/tests/test_accounts_views.py
+++ accounts/tests/test_accounts_views.py
@@ -1,2 +1,9 @@
-def test_dummy():
-    assert True
+from django.urls import reverse
+
+def test_login_page_loads(client):
+    response = client.get(reverse("login"))
+    assert response.status_code == 200
+
+def test_register_page_loads(client):
+    response = client.get(reverse("register"))
+    assert response.status_code == 200
--- accounts/tests/test_views.py
+++
@@ -1,9 +0,0 @@
-from django.urls import reverse
-
-def test_login_page_loads(client):
-    response = client.get(reverse("login"))
-    assert response.status_code == 200
-
-def test_register_page_loads(client):
-    response = client.get(reverse("register"))
-    assert response.status_code == 200
