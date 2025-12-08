434f9ff640b4572caa118e98c4944e7399ada5dc
shri0612 <shri2178499@gmail.com>
2025-12-08 02:00:09 +0000

    testing 1

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/shri0612/job-application-tracker/env
+set -gx VIRTUAL_ENV /home/shri0612/devops/job-application-tracker/env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
